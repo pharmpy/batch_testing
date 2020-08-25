419c121346e2297e858e6f903d7fa41a06b757db
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-08-25 15:37:32 +0200

    Make runnable with pytest

--- README.rst
+++ README.rst
@@ -1,5 +1,10 @@
 .. start-longdesc
 
 Scripts for testing Pharmpy with batches of models.
+Run from pharmpy directory with
+
+.. code::
+
+tox -e py38 -- pytest -s ../batch_testing/parser.py
 
 .. end-longdesc
--- parser.py
+++ parser.py
@@ -2,38 +2,42 @@ from pathlib import Path
 
 from pharmpy import Model
 from pharmpy.plugins.utils import PluginError
-from pharmpy.model_transformation import explicit_odes
+from pharmpy.modeling import explicit_odes
 
 model_dir = Path('/home/rikard/devel/scripts/pharmpy_testing/models')
 #model_dir = Path('/home/rikard/devel/scripts/pharmpy_testing/ddmore')
 
 n = 0
 failed = 0
-for f in model_dir.iterdir():
-    n += 1
-    try:
-        model = Model(f)
-    except PluginError:
-        continue
-    print(model.name)
 
-    # Test round-trip
-    with open(f, 'r', encoding='latin-1') as fh:
-        content = fh.read()
-    assert content == str(model)
 
-    # Parse parameters
-    params = model.parameters
+def test_models():
+    global n, failed
+    for f in model_dir.iterdir():
+        n += 1
+        try:
+            model = Model(f)
+        except PluginError:
+            continue
+        print(model.name)
 
-    # Parse PK/PRED/ERROR
-    try:
-        statements = model.statements
-    except Exception as e:
-        print(e)
-        failed += 1
+        # Test round-trip
+        with open(f, 'r', encoding='latin-1') as fh:
+            content = fh.read()
+        assert content == str(model)
 
-    # Test explicit_odes transformation
-    explicit_odes(model)
+        # Parse parameters
+        params = model.parameters
+
+        # Parse PK/PRED/ERROR
+        try:
+            statements = model.statements
+        except Exception as e:
+            print(e)
+            failed += 1
+
+        # Test explicit_odes transformation
+        explicit_odes(model)
 
 print(f'{n} models tested')
 print(f'{failed} models failed in statements')

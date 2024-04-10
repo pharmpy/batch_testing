caa9193a320517c40d3bdc85272f021942468be8
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-04-10 14:16:05 +0200

    Remove unnecessary and broken tests

--- parser.py
+++ parser.py
@@ -1,35 +1,16 @@
-#import argparse
 from pathlib import Path
 
 import pytest
 
-from pharmpy import Model
-from pharmpy.plugins.utils import PluginError
-from pharmpy.modeling import explicit_odes
+from pharmpy.modeling import read_model
 
 
 def test_models(model_path):
     f = model_path
-    try:
-        model = Model(f)
-    except PluginError:
-        return
+    model = read_model(f)
     print(model.name)
 
     # Test round-trip
-    with open(f, 'r', encoding='latin-1') as fh:
-        content = fh.read()
-    assert content == str(model)
-
-    # Parse parameters
-    params = model.parameters
-
-    # Parse PK/PRED/ERROR
-    try:
-        statements = model.statements
-    except Exception as e:
-        print(e)
-
-
-def test_explicit_odes(model):
-    explicit_odes(model)
+    #with open(f, 'r', encoding='latin-1') as fh:
+    #    content = fh.read()
+    #assert content == str(model)

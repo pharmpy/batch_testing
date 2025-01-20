441bc97af2ae6b814b479050484801f67283d39b
Rikard Nordgren <rikard.nordgren@uu.se>
2025-01-20 16:38:58 +0100

    Nicer table output from extract_validations

--- extract_validations.py
+++ extract_validations.py
@@ -1,11 +1,26 @@
 from pathlib import Path
 
+from rich.console import Console
+from rich.table import Table
+
 test_path = Path("tests")
 
+table = Table(title="All validation tests")
+table.add_column("Function", justify="right", style="cyan", no_wrap=True)
+table.add_column("Validation", style="magenta")
+
+validations = []
 for path in test_path.glob("test_*.py"):
     with open(path, "r") as f:
         for line in f:
             line = line.strip()
             if line.startswith("# validate: "):
                 validation = line[12:]
-                print("*", validation)
+                validations.append(validation)
+
+for v in sorted(validations):
+    a = v.split(maxsplit=1)
+    table.add_row(a[0], a[1])
+
+console = Console()
+console.print(table)

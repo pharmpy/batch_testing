116d4049cf12195bca24b584800277507181048f
Rikard Nordgren <rikard.nordgren@uu.se>
2025-09-26 13:14:41 +0200

    Test repr of whole statements instead of only ode system

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -1,17 +1,8 @@
 from pharmpy.modeling import read_model
 
-import re
-
-def find_in_code(model, regex):
-    m = re.search(regex, model.code, re.MULTILINE)
-    return m
-
 
 def test_repr(model_path):
     model = read_model(model_path)
 
-    have_pk = find_in_code(model, r'^\s*\$PK')
-    if have_pk:
-        print(model.name)
-        # validate: repr for ODE system does not crash
-        assert repr(model.statements.ode_system)
+    # validate: repr for model.statements produces something
+    assert repr(model.statements)
